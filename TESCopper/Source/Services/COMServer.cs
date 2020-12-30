ecd3e45e9a416774487126483ce4a9b369856a00
Brandon Cook <Brandon@Cook>
2020-12-30 16:45:21 -0600

    Started On Server Side Programming. Working Multi Client Enviorment to allow for mutiple clients to send commands.

--- TESCopper/Source/Services/COMServer.cs
+++ TESCopper/Source/Services/COMServer.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace TESCopper
 {
+
     class COMServer
     {
+        private bool isListening = false;
+        private ManualResetEvent mREvent = new ManualResetEvent(false);
+
         public COMServer()
         {
-            IPAddress ipAddr = new IPAddress(new byte[]{ 127,0,0,1});
+            IPAddress ipAddr = new IPAddress(new byte[] { 127, 0, 0, 1 });
             IPEndPoint localEndpoint = new IPEndPoint(ipAddr, 8888);
 
             Socket listener = new Socket(
                 ipAddr.AddressFamily,
                 SocketType.Stream,
                 ProtocolType.Tcp);
-            
+
             try
             {
                 listener.Bind(localEndpoint);
@@ -26,14 +32,14 @@ namespace TESCopper
 
                 while (true)
                 {
-                    Console.WriteLine("Waitingg for Connection");
+                    Console.WriteLine("Waiting for Connection");
 
                     Socket clientSocket = listener.Accept();
 
                     byte[] bytes = new byte[1024];
                     string data = null;
 
-                    while(true)
+                    while (true)
                     {
                         int numByte = clientSocket.Receive(bytes);
 
@@ -58,5 +64,76 @@ namespace TESCopper
             }
 
         }
+
+        public Task StartListening_Ipv4(int port, byte[] ipAddress)
+        {
+            return Task.Run(() =>
+            {
+
+                byte[] dataBuffer = new byte[1024]; 
+                IPHostEntry ipHostInfo = Dns.EndGetHostEntry(null);
+                IPAddress ipAddress = ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+                Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                isListening = true;
+
+
+                try
+                {
+                    listener.Bind(localEndPoint);
+                    listener.Listen(100);
+
+                    while (isListening)
+                    {
+                        mREvent.Reset();
+
+                        listener.BeginAccept(new AsyncCallback(AcceptCallBack),listener);
+
+                        mREvent.WaitOne();
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    listener.Disconnect(true);
+                    mREvent.Close();
+                    Console.WriteLine("Connection Broken Due To Error");
+                }
+
+            });
+        }
+
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            mREvent.Set();
+
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            StateObject state = new StateObject();
+            state.WorkerSocket = handler;
+
+            handler.BeginReceive(state.buffer,0, StateObject.bufferSize,0,
+                new AsyncCallback(ReadCallback), state);
+
+
+        }
+        private void ReadCallback(IAsyncResult ar)
+        { }
+
+    }
+
+    public partial class StateObject
+    {
+        public Socket WorkerSocket { get; set; }
+        public const int bufferSize = 1024;
+        public byte[] buffer = new byte[bufferSize];
+        public StringBuilder recieverString { get; set; }
     }
 }
