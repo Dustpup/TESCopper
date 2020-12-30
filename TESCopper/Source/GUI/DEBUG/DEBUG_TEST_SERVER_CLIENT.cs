b7ac522f1900aca9ff0458fd61b6c8cf8649088e
Brandon Cook <Brandon@Cook>
2020-12-29 23:24:37 -0600

    Server Test

---
+++ TESCopper/Source/GUI/DEBUG/DEBUG_TEST_SERVER_CLIENT.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TESCopper
+{
+    class DEBUG_TEST_SERVER_CLIENT
+    {
+        enum TESTS
+        {
+            SERVER,
+            CLIENT
+        }
+        public DEBUG_TEST_SERVER_CLIENT()
+        {
+            Console.WriteLine("Starting Server and Client Testing...");
+
+            byte counter = 0;
+            bool isRunning = true;
+
+            foreach (var option in Enum.GetValues(typeof(TESTS)))
+            {
+                Console.WriteLine("\t{1}:[{0}]", option, counter);
+                counter++;
+            }
+            Console.WriteLine("\tQ:Quite SEVER CLIENT DEBUG");
+            Console.WriteLine("\tH:Help");
+
+            while (isRunning)
+            {
+                try
+                {
+                    char usrInpChar = GetKeyWMsg(": SER CLI TES:>").KeyChar;
+                    Console.WriteLine();
+
+                    switch (usrInpChar)
+                    {
+                        case 'q':
+                            isRunning = false;
+                            break;
+                        case 'h':
+                            foreach (var option in Enum.GetValues(typeof(TESTS)))
+                            {
+                                Console.WriteLine("\t{1}:[{0}]", option, counter);
+                                counter++;
+                            }
+                            Console.WriteLine("\tQ:Quite XML DEBUG");
+                            Console.WriteLine("\tH:Help");
+                            break;
+                        default:
+                            TESTS testSelection = TESTS.CLIENT;
+                            bool checkSelString = Enum.TryParse<TESTS>(usrInpChar.ToString(), true, out testSelection);
+                            if (checkSelString)
+                                switch (testSelection)
+                                {
+                                    case TESTS.CLIENT:
+                                        Console.WriteLine("\t\t Starting Communications Client..");
+                                        COMClient client = new COMClient();
+                                        break;
+                                    case TESTS.SERVER:
+                                        COMServer server = new COMServer();
+                                        break;
+                                }
+                            else throw new Exception("Could Not Find Test");
+                            
+                            break;
+
+
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private string GetInputMsg(string msg)
+        {
+            Console.Write(msg);
+            return Console.ReadLine();
+        }
+        private ConsoleKeyInfo GetKeyWMsg(string msg)
+        {
+            Console.Write(msg);
+            return Console.ReadKey();
+        }
+        private bool GetKeyMsg(string msg, ConsoleKey correctKey)
+        {
+            Console.Write(msg);
+            return (Console.ReadKey().Key == correctKey ? true : false);
+        }
+    }
+}
