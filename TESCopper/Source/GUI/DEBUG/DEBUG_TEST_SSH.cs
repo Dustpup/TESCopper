6ca36c96e37329d411681facf0e5aa7ade3e2cfa
Brandon Cook <Brandon@Cook>
2021-04-21 20:08:42 -0500

    DEBUG

---
+++ TESCopper/Source/GUI/DEBUG/DEBUG_TEST_SSH.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TESCopper
+{
+    class DEBUG_TEST_SSH
+    {
+        SshService service = new SshService();
+
+        public DEBUG_TEST_SSH()
+        {
+            Console.WriteLine("Testing SSH...");
+            service.Init(ASCIIEncoding.ASCII.GetBytes("TEST"),
+                "TEST","123.456.123.132");
+
+            service.AddMultiAddress(new string[] {
+                "123.123.123.123",
+                "147.147.147.147",
+                "159.159.159.159" },
+                new string[] { "BANNED", "HACKER"});
+        }
+    }
+}
